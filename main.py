9f142e617baa17d2d688c0b945ee713d87cfb2e0
Austin Brown <austinbrown@Austins-MBP.home>
2018-10-29 13:45:36 -0400

    added 50 more

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import requests
 from flask import Flask
-#from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
 import json
 
 from parser import scrape_page
 
 app = Flask(__name__)
-#cors = CORS(app)
-#app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 def prepare_data():
     URL = "https://masteroverwatch.com/leaderboards/pc/global/mode/ranked/category/skillrating/hero/overall/role/overall/data"
-#    offsets = ["50"]
     entries = []
     offsets = []
-    for i in range(1, 6):
+    for i in range(0, 6):
         offsets.append(i * 50)
     for i in offsets:
         r = requests.get(url = URL, params = {'offset': i})
@@ -23,7 +22,7 @@ def prepare_data():
     return entries
 
 @app.route("/api")
-#@cross_origin()
+@cross_origin()
 def main():
     entries = prepare_data()
     names = scrape_page(entries)
