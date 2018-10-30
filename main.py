5d527f4c9d691bc908680a42acbbf0f5b8ad96e2
Austin Brown <austinbrown@Austins-MBP.home>
2018-10-29 23:37:15 -0400

    changes to api

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 import json
 
-from parser import scrape_page
+from parser import get_player_info
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,19 +11,11 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 def prepare_data():
     URL = "https://masteroverwatch.com/leaderboards/pc/global/mode/ranked/category/skillrating/hero/overall/role/overall/data"
-    entries = []
-    offsets = []
-    for i in range(0, 6):
-        offsets.append(i * 50)
-    for i in offsets:
-        r = requests.get(url = URL, params = {'offset': i})
-        data = r.json()
-        entries.append(data['entries'])
-    return entries
+    return [requests.get(url = URL, params = {'offset': i}).json()['entries'] for i in [j * 50 for j in range(6)]]
 
 @app.route("/api")
 @cross_origin()
 def main():
     entries = prepare_data()
-    names = scrape_page(entries)
-    return json.dumps({'names': names})
+    players = get_player_info(entries)
+    return json.dumps({'players': players})
--- parser.py
+++ parser.py
@@ -1,11 +1,26 @@
 from bs4 import BeautifulSoup
 
-def scrape_page(entries):
-    names = []
+def get_skill_rating(row):
+    return row.find("div", class_="table-main-value").strong.text.strip()
+
+def get_name(row):
+    return row.find("span", class_="table-name-block").strong.text.strip()
+
+def get_tag(row):
+    return row.find("a", class_="table-row-link").get("href").split('-')[1]
+
+def get_player_info(entries):
+    players = []
     for entry in entries:
         for document in entry:
             soup = BeautifulSoup(document, 'html.parser')
-            for row in soup.find_all("span", class_="table-name-block"):
-                names.append(row.strong.text.strip())
-    return names    
-        
+            for row in soup.find_all("div", class_="table-row"):
+                attributes = {}
+                name = get_name(row)
+                tag = get_tag(row)
+                skill_rating = get_skill_rating(row)
+                attributes["name"] = name
+                attributes["tag"] = tag
+                attributes["skill_rating"] = skill_rating
+                players.append(attributes)
+    return players
